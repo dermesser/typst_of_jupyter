c4200c17a0b093e41e58e85424209256749b179d
Lewin Bormann <lbo@spheniscida.de>
2023-07-26 12:09:27 +0200

    Typst: implement conversion for def. list, links, images

--- bin/Typst.ml
+++ bin/Typst.ml
@@ -28,25 +28,48 @@ open Util
 open Base
 open Omd
 
-let inline_to_typst buf = function
+let rec inline_to_typst buf = function
   | Text (_attr, text) -> Buffer.add_string buf text
+  | Concat (_attr, inls) -> List.iter ~f:(inline_to_typst buf) inls
+  | Image (_attr, { label; destination; title }) ->
+      let label =
+        let buf2 = Buffer.create 512 in
+        inline_to_typst buf2 label;
+        Buffer.contents buf2
+      in
+      let s =
+        Printf.sprintf {|#figure(image("%s", width: 80%%), caption: "%s")
+        |}
+          destination label
+      in
+      Buffer.add_string buf s
+  | Link (_attr, { label; destination; title }) ->
+      let label =
+        let buf2 = Buffer.create 512 in
+        inline_to_typst buf2 label;
+        Buffer.contents buf2
+      in
+      let s = Printf.sprintf {|#link("%s")[%s]
+      |} destination label in
+      Buffer.add_string buf s
   | _ -> raise Unimplemented
 
 let markdown_to_typst md =
   let buf = Buffer.create 1024 in
   let endl () = Buffer.add_char buf '\n' in
+  let s str = Buffer.add_string buf str in
+  let c ch = Buffer.add_char buf ch in
   (* Convert single block to typst by writing to buffer. *)
   let rec block_to_typst buf = function
-    | Paragraph (attr, inl) ->
-        inline_to_typst buf inl
+    | Paragraph (attr, inl) -> inline_to_typst buf inl
     | List (_attr, lt, _ls, blocklist_list) ->
         let bullet =
           match lt with
           | Ordered (i, c) -> Printf.sprintf "%d%c " i c
-          | Bullet c -> Char.to_string c
+          | Bullet c -> Printf.sprintf "%c " c
         in
         let write_listitem_block block =
-          Buffer.add_string buf bullet;
+          s bullet;
           block_to_typst buf block;
           endl ()
         in
@@ -55,26 +78,35 @@ let markdown_to_typst md =
     | Blockquote (_attr, blocklist) ->
         (* todo: find best typst equivalent *)
         List.iter ~f:(block_to_typst buf) blocklist
-    | Thematic_break attr ->
-        Buffer.add_string buf "#line(length: 100%, stroke: gray)\n"
+    | Thematic_break attr -> s "#line(length: 100%, stroke: gray)\n"
     | Heading (attr, level, inl) ->
         let marker =
           String.concat
             (Sequence.to_list (Sequence.take (Sequence.repeat "=") level))
         in
-    endl ();
-        Buffer.add_string buf marker;
-        Buffer.add_string buf " ";
+        s marker;
+        s " ";
         inline_to_typst buf inl;
         endl ()
     | Code_block (attr, lang, contents) ->
-        Buffer.add_string buf "```";
-        Buffer.add_string buf lang;
-        Buffer.add_char buf '\n';
-        Buffer.add_string buf contents;
-        Buffer.add_string buf "```";
+        endl ();
+        s "```";
+        s lang;
+        c '\n';
+        s contents;
+        s "```"
     | Html_block (attr, s) -> raise Unimplemented
-    | Definition_list (attr, defs) -> raise Unimplemented
+    | Definition_list (_attr, defs) ->
+        let write_term { term; defs } =
+          (* assert (Int.equal 1 (List.length defs)); *)
+          s "/ ";
+          inline_to_typst buf term;
+          s ": ";
+          inline_to_typst buf (List.hd_exn defs);
+          endl ()
+        in
+        List.iter ~f:write_term defs;
+        endl ()
     | Table (attr, head, cells) -> raise Unimplemented
   in
   List.iter
@@ -82,5 +114,5 @@ let markdown_to_typst md =
       block_to_typst buf block;
       endl ())
     md;
-  Buffer.add_char buf '\n';
+  c '\n';
   Buffer.contents buf
--- bin/tests/Typst_test.ml
+++ bin/tests/Typst_test.ml
@@ -15,6 +15,20 @@ Hello World
 * item
 * item 2
 
+### A definition list!
+
+Term 1
+: That's the first term
+
+Term 2
+: The second term
+
+## Images and links
+
+![An image alt text](test.png)
+
+[Linky link](https://borgac.net)
+
 And this is it.
 
 |}
@@ -24,23 +38,33 @@ let%expect_test _ =
   [%expect {|
     = Markdown example
 
-
     == A paragraph
 
     Hello World, this is a paragraph.
-
     == A code block
 
     This is a code block:
+
     ```ocaml
     Hello World
     ```
-
     == A list
 
-    *A list
-    *item
-    *item 2
+    * A list
+    * item
+    * item 2
+
+    === A definition list!
+
+    / Term 1: That's the first term
+    / Term 2: The second term
+
+
+    == Images and links
+
+    #figure(image("test.png", width: 80%), caption: "An image alt text")
+
+    #link("https://borgac.net")[Linky link]
 
     And this is it. |}]
   
