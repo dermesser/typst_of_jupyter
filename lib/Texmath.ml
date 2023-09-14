cb5f5b0977c18eb1cc0eee385d726fdfc0c3f8bf
Lewin Bormann <lbo@spheniscida.de>
2023-09-14 21:39:58 +0200

    Texmath: more robust parsing

--- lib/Texmath.ml
+++ lib/Texmath.ml
@@ -10,6 +10,12 @@ type texpression =
   | Token of string
 [@@deriving sexp]
 
+type result = Ok of texpression | Error of { pos : int; reason : string }
+
+let failwith reason =
+  let+ pos = pos in
+  Error { pos; reason }
+
 let chain f g x = f (g x)
 let negate f = chain not f
 
@@ -17,24 +23,50 @@ let token_char c =
   (not (Char.is_whitespace c))
   && not (List.mem ~equal:Char.equal [ '{'; '}'; '['; ']'; '('; ')' ] c)
 
-let token = take_while token_char >>| fun t -> Token t
+let token = take_while1 token_char >>| fun t -> Token t
 let macroname = char '\\' *> take_while Char.is_alpha
+let skip_whitespace = skip_while Char.is_whitespace
 
-let rec block e = char '{' *> e <* char '}'
+let rec block e = char '{' *> e <* char '}' <?> "block"
 
 and macro1 e =
   let* mn = macroname in
   let+ arg = block e in
   Macro1 (mn, arg)
 
+and macro2 e =
+  let* mn = macroname in
+  let* arg1 = block e in
+  let+ arg2 = block e in
+  Macro2 (mn, arg2, arg2)
+
 let macro0 = macroname >>| fun mn -> Macro0 mn
-let expression = fix (fun e -> choice [ block e; macro1 e; macro0; token ])
+
+let expression =
+  fix (fun e -> choice [ block e; macro2 e; macro1 e; macro0; token ])
+
+let expressions =
+  sep_by1 (satisfy Char.is_whitespace) expression <* skip_whitespace
+
+let parse_and_dump expr =
+  let dump t = sexp_of_texpression t |> Sexp.to_string in
+  let e = parse_string ~consume:Angstrom.Consume.All expressions expr in
+  match e with
+  | Ok ts ->
+      Printf.printf "%s\n" (String.concat ~sep:"~~" (List.map ~f:dump ts))
+  | Error s -> Printf.printf "error for \"%s\": %s\n" expr s
 
 let%expect_test "simple-expression" =
-  (match
-    parse_string ~consume:Angstrom.Consume.All expression "\\hello{world}"
-  with
-  | Ok t -> Printf.printf "%s" (Sexp.to_string (sexp_of_texpression t))
-  | Error s ->
-      Printf.printf "error: %s" s);
-  [%expect {| (Macro1 hello(Token world)) |}]
+  let exprs =
+    [
+      {|\macro{input}|};
+      {|\macro{input1}{input2} |};
+      {|\macro{input1} {input2}|};
+    ]
+  in
+  List.iter ~f:parse_and_dump exprs;
+  [%expect
+    {|
+    (Macro1 macro(Token input))
+    (Macro2 macro(Token input2)(Token input2))
+    (Macro1 macro(Token input1))~~(Token input2) |}]
